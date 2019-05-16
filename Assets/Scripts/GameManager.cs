09b94280bb65d05af500da8b364ccfce807b3520
Unknown <alecsantos96@gmail.com>
2019-05-16 17:21:13 -0300

    Movimento e pontuação
    e upload da imagem do menu principal

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private Text pText;
+    public float points;
+
+    void Start()
+    {
+        pText = FindObjectOfType<Text>().GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pText.text = "Points: " + points.ToString("F0");
+    }
+
+    void FixedUpdate()
+    {
+        points += Time.deltaTime * 10;
+    }
+}
--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool ground;
+    private Rigidbody rb;
+    private bool moving;
+    private float moveX = 10;
+    private float moveY = 10;
+    private bool left, right;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!moving)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && !left)
+            {
+                rb.AddForce(-moveX, 0, 0, ForceMode.Impulse);
+                if (right)
+                    right = false;
+                else
+                    left = true;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow) && !right)
+            {
+                rb.AddForce(moveX, 0, 0, ForceMode.Impulse);
+                if (left)
+                    left = false;
+                else
+                    right = true;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow) && ground)
+            {
+                rb.AddForce(0, moveY, 0, ForceMode.Impulse);
+                ground = false;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && ground)
+                transform.localScale = new Vector3(1, .5f, 1);
+            else
+                transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+            moving = true;
+        else
+            moving = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "chao")
+            ground = true;
     }
 }
